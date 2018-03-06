5e0fba1a2a878de42fd095cfcdae32521b75dae8
ZainAnjum <zainanjum100@gmail.com>
2018-03-06 22:13:09 +0500

    initial

--- IMageSlider/ViewController.swift
+++ IMageSlider/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.contentSize = CGSize(width: 12, height: 200)
+        sv.backgroundColor = .white
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.isPagingEnabled = true
+        return sv
+    }()
+    var imageArray = [UIImage]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageArray = [#imageLiteral(resourceName: "4"),#imageLiteral(resourceName: "2"),#imageLiteral(resourceName: "1"),#imageLiteral(resourceName: "3")]
+        scrollView.frame = view.frame
+        view.addSubview(scrollView)
+        for i in 0..<imageArray.count{
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: view.frame.width, height: 200)
+            
+            scrollView.contentSize.width = scrollView.frame.width * CGFloat(i + 1)
+            scrollView.addSubview(imageView)
+            
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
